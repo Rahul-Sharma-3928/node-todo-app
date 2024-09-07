915020272b8db57c5fb3b3916071a2e86e46dd20
Rahul Sharma <rahul@kali.Rahul>
2024-09-07 15:40:45 +0530

    change origin path

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use("/api/v1/users", userRoute);
 app.use("/api/v1/tasks", taskRoute);
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
